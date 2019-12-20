1dad7746c1fb5cde3964cef380e81a64446ee10f
Sune <sune.bro@ka-net.dk>
2019-12-20 12:07:15 +0100

    Løst opgave 5

--- src/Person.java
+++ src/Person.java
@@ -8,27 +8,28 @@
 public class Person {
 
     // TODO Ret nedenstående datatyper
-    String navn; // Test Testesen
-    String alder; // 18
-    String by; // Næstved
-    String postNummer; // 4700
-    String vej; // Femøvej
-    String husnummer; // 3B
-    String nationalitet; // Dansk
-    String fritidsStringeresser; // Snitning, LOL, strikke
-    String smarthomeDevices; // Xbox, 3 Smartlamper, Køleskab m.m.
-    String hojde; // 1.56 m
-    String vaegt;  // 76 kg
-    String karaktergennemsnit; // 5.33
-    String eksamensKarakterer; // 7, 12, -3, 4, 2, 10
-    String opdateretDato; // Sun Jan 08 00:00:00 CET 1978
+    String navn= "Test Testesen";
+    byte alder = 18;
+    String by = "Næstved";
+    int postNummer= 4700;
+    String vej = "Femøvej";
+    String husnummer = "3B";
+    String nationalitet = "Dansk";
+    String fritidsStringeresser = "Snitning, LOL, strikke";
+    String smarthomeDevices = "Xbox, 3 Smartlamper, Køleskab m.m."
+    String hojde = "1.56 m";
+    String vaegt = "76 kg";
+    double karaktergennemsnit =5.33;
+    //short eksamensKarakterer = (int) (7, 12, -3, 4,  2, 10); giver unexpected token error?
+    String opdateretDato = "Sun Jan 08 00:00:00 CET 1978";
 
-    // TODO Vis eksempler på typecasting
+
     @Override
     public String toString() {
         return "Navn: " + navn + "By" + by;
     }
-
+    //eksempel på typecasting fra short til int
+    short i = (int) 200;
     public static void main(String[] args) {
 
         Person person = new Person();
